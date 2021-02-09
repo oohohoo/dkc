ef2da6f508108c5cb8278317ea0516366ac4bc1d
dejan-77 <dejankutic@me.com>
2021-02-09 06:35:49 +0100

    dheu

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ JQUERY NTH GRANDFADER - CHILD
     
 
     //This adds the class to all matches as well
-    $('ul > li > ul > li > div').addClass('great-great-grandchild');
+    $('a > ul > li > ul > li > div').addClass('great-great-grandchild');
     
     // element:first selector selects the first occurrence of a given element  
     console.log($('ul:first').attr("value")); //Returns the value attribute of the first <ul> element
