58818d27d1d973263d3e941d287fc6cf669e254d
dejan-77 <dejankutic@me.com>
2021-02-10 09:32:54 +0100

    fe

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ FUNCTION INIT
 */
 function init() {
 
-
-
 /*
 ================================================================================
 JQUERY NTH GRANDFADER - CHILD
@@ -17,7 +15,7 @@ JQUERY NTH GRANDFADER - CHILD
 
     // Using Jquery $( "parent > child" ); selector which returns an array of matches  
     // Iterate through an array of Jquery objects using $(..).each()
-    $('ul > li > ul > li').each(function(){
+    $('ul > li > a > ul > li').each(function(){
     
       console.log($(this).text());
       $(this).addClass('great-great-grandchild');
