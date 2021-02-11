3a22f7c4dead4a2b9b6a3549cd6e70d8fd52ab7b
dejan-77 <dejankutic@me.com>
2021-02-11 08:45:05 +0100

    gr

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ JQUERY NTH GRANDFADER - CHILD
 
     // Using Jquery $( "parent > child" ); selector which returns an array of matches  
     // Iterate through an array of Jquery objects using $(..).each()
-    $('ul > li > ul > li > div').each(function(){
+    $('ul > li > ul > li ').each(function(){
     
       console.log($(this).text());
       $(this).addClass('newname');
@@ -24,7 +24,7 @@ JQUERY NTH GRANDFADER - CHILD
     
 
     //This adds the class to all matches as well
-    $('ul > li > a > ul > li').addClass('newname');
+    $('a > ul > li > ul > li').addClass('newname');
     
     // element:first selector selects the first occurrence of a given element  
     console.log($('ul:first').attr("value")); //Returns the value attribute of the first <ul> element
