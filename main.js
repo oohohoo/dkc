980c3b0378f67498f751220fa5810ba0facc3fdf
oohohoo <31392278+oohohoo@users.noreply.github.com>
2024-06-21 14:56:20 +0200

    2024 / update / swiper

--- main.js
+++ main.js
@@ -269,60 +269,44 @@ SWIPER HERO
 ================================================================================
 */
 
-var menu = ['', '', '', '', '', '']  
-  var mySwiper = new Swiper ('.swiper-container', {
-   loop: true,
-		speed: 1000,
-   // spaceBetween: 100,
-   // initialSlide: 0,
-    //truewrapper adoptsheight of active slide
+var menu = ['', '', '', '', '', ''];
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    speed: 1000,
     autoHeight: false,
     updateOnWindowResize: true,
-    // Optional parameters
-  //  direction: 'vertical',
-      // delay between transitions in ms
-    	autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-},
-   // autoplayStopOnLast: false, // loop false also
-    // If we need pagination
- 
-    
-    // Navigation arrows
- /*   nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',*/
-    
-    // And if we need scrollbar
-    //scrollbar: '.swiper-scrollbar',
-    // "slide", "fade", "cube", "coverflow" or "flip"
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     effect: 'fade',
-    
-      pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-				type: 'bullets',
-				renderBullet: function (index, className) {
-					return '<span class="' + className + '">' + (menu[index]) + '</span>';
-},
-    // Distance between slides in px.
-  //  spaceBetween: 60,
-    //
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+    },
     slidesPerView: 1,
     observer: true,
-  observeParents: true,
-  observeSlideChildren: true,
-  longSwipes: false,
-  
-    //
-   // centeredSlides: true,
-    //
-   // slidesOffsetBefore: 0,
-    //
+    observeParents: true,
+    observeSlideChildren: true,
+    longSwipes: false,
     grabCursor: true,
-  }
-  })        
-  
+    on: {
+        slideChangeTransitionStart: function () {
+            // Add fade-out class to all .up elements in the previous slide
+            var previousSlide = this.slides[this.previousIndex];
+            $(previousSlide).find('.up').addClass('fade-out');
+        },
+        slideChangeTransitionEnd: function () {
+            // Remove fade-out class from all .up elements in the previous slide
+            var previousSlide = this.slides[this.previousIndex];
+            $(previousSlide).find('.up').removeClass('fade-out');
+        }
+    }
+});
 /*
 ================================================================================
 REFRESH PAGE ON SCREEN ROTATE
